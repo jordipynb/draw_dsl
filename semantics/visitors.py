4754e62616ba799e4f3978abe85afece73922b4b
altmoket <copy90471@gmail.com>
2022-12-31 04:05:05 -0500

    fix: Incomplete semantic checker

--- semantics/visitors.py
+++ semantics/visitors.py
@@ -1,5 +1,6 @@
 from semantics import visitor
-from utils import Axiom, Rule, Scene, Draw, Shape, Value
+from semantics.scope import Scope
+from utils import Axiom, Nill, Rule, Scene, Draw, Shape, Value
 S = '|  '
 class FormatVisitor(object):
     @visitor.on('node')
@@ -30,13 +31,18 @@ class FormatVisitor(object):
         axiom = self.visit(node.axiom, tabs + 1)
         properties = "\n".join([name, pencil, rules, axiom])
         return f'{ans}\n{properties}'
+    
+    @visitor.when(Nill)
+    def visit(self, node, tabs=0):
+        ans = S*tabs + f'nill'
+        return f'{ans}'
 
     @visitor.when(Value)
     def visit(self, node, tabs=0, header=''):
         ans = S*tabs + f'{header}: {node.value}'
         return f'{ans}'
     
-    @visitor.when(Rule)
+    @visitor.when(Rule) # Falta una parte de las reglas
     def visit(self, node, tabs=0):
         ans = S*tabs + f'Rule:'
         name = S*(tabs+1) + f'name: {node.name}'
@@ -44,7 +50,51 @@ class FormatVisitor(object):
         properties = "\n".join([name, param])
         return f'{ans}\n{properties}'
     
-    @visitor.when(Axiom)
+    @visitor.when(Axiom) # Falta este
     def visit(self, node, tabs=0):
         ans = S*tabs + f'axiom'
         return f'{ans}'
+    
+class SemanticCheckerVisitor(object):
+    def __init__(self):
+        self.errors = []
+    
+    @visitor.on('node')
+    def visit(self, node, scope):
+        pass
+    
+    @visitor.when(Scene)
+    def visit(self, node, scope=None):
+        scope = Scope(scope)
+        
+        for draw in node.draws:
+            self.visit(draw, scope)
+            
+        return self.errors
+    
+    @visitor.when(Draw)
+    def visit(self, node:Draw, scope:Scope=None):
+        self.visit(node.shape, scope)
+        self.visit(node.x)
+        self.visit(node.y)
+
+    @visitor.when(Shape)
+    def visit(self, node:Shape, scope:Scope=None):
+        if scope.is_shape_defined(node.name):
+            self.errors.append("Shape %s is already defined" %(node.name))
+        else:
+            nscope = scope.define_shape(node.name) 
+            # self.visit(node.pencil, nscope) # Definir pencil como una clase
+            for rule in node.rules:
+                self.visit(rule, nscope)
+            self.visit(node.axiom, nscope)
+    
+    @visitor.when(Rule)
+    def visit(self, node:Rule, scope:Scope=None):
+        if scope.is_rule_defined(node.name):
+            self.errors.append("Shape %s is already defined" %(node.name))
+        else:
+            for rule in node.rules:
+                self.visit(rule, None) # No es necesario definir scope
+            self.visit(node.axiom, None) # No es necesario definit scope
+    
